f6fd2ae4ca6ed56d5648659829d3ced662b16737
Pierre Vincent <pvincent@poppulo.com>
2017-08-29 23:16:41 +0100

    Use Grafana Dashboard Import API Endpoint

--- grafanaUpdater.go
+++ grafanaUpdater.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"strings"
 	"errors"
+	"encoding/json"
+	"log"
 )
 
 type GrafanaUpdater struct {
@@ -18,14 +20,32 @@ func NewGrafanaUpdater(url string, username string, password string) *GrafanaUpd
 	return &GrafanaUpdater{
 		username: username,
 		password: password,
-		dashboardApiUrl: fmt.Sprintf("%s/api/dashboards/db", url),
+		dashboardApiUrl: fmt.Sprintf("%s/api/dashboards/import", url),
 		datasourceApiUrl: fmt.Sprintf("%s/api/datasources", url),
 	}
 }
 
 func (updater *GrafanaUpdater) PushDashboard(dashboardJson string) error {
-	dashboardPostBody := fmt.Sprintf("{ \"dashboard\":%s, \"overwrite\": true }", dashboardJson)
+	dashboardPostBody, err := buildDashboardPushBody(dashboardJson)
+	if err != nil {
+		return err
+	}
 	return grafanaApiPost(updater.dashboardApiUrl, dashboardPostBody)
+}
+
+func buildDashboardPushBody(dashboardJson string) (string, error) {
+	var m map[string]interface{}
+	err := json.Unmarshal([]byte(dashboardJson), &m)
+	if err != nil {
+		return "", err
+	}
+	if m["dashboard"] != nil {
+		return dashboardJson, nil
+	} else {
+		log.Println("No 'dashboard' key, wrapping in Dashboard Import object")
+		return fmt.Sprintf("{ \"dashboard\":%s, \"overwrite\": true }", dashboardJson), nil
+	}
+
 }
 
 func (updater *GrafanaUpdater) PushDatasource(datasourceJson string) error {
